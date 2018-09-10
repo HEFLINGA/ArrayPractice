16b4ff976dba2b43cb946f7e91a69516788293c9
Auston Hefling <auston.hefling@outlook.com>
2018-09-10 11:19:45 -0400

    Array practice

---
+++ ArraySept10/ArrayPractice.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySept10
+{
+    class ArrayPractice
+    {
+        public void ShowOutPut(int[] arr)
+        {
+            var l = arr.Length;
+
+            Console.Write("OutPut: ");
+
+            if (l == 0)
+            {
+                Console.WriteLine($"{{ {arr[0]} }}");
+            }
+            else if (l > 0)
+            {
+                Console.Write($"{{ {arr[0]}");
+
+                for (int i = 1; i < l; ++i)
+                {
+                    Console.Write($", {arr[i]}");
+                }
+
+                Console.WriteLine(" }");
+            }
+            else
+            {
+                Console.WriteLine(" { }");
+            }
+
+            Console.WriteLine();
+        }
+
+        // Write array that swaps x with y and y with x
+        public void Swap(int[] arr, int x, int y)
+        {
+            var l = arr.Length;
+
+            if (x >= 0 && x < l && y >= 0 && y < l)
+            {
+                var tmp = arr[x];
+
+                arr[x] = arr[y];
+                arr[y] = tmp;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        } 
+
+        public void ReverseInPlace(int[] arr)
+        {
+            var l = arr.Length;
+
+            var last = l - 1;
+            var middle = l / 2;
+
+            for (int i = 0; i < middle; ++i)
+            {
+                this.Swap(arr, i, last - i);
+            }
+        }
+
+        public int[] ReverseNew(int[] arr)
+        {
+            int[] newArr = new int[arr.Length];
+
+            for (int oIndex = arr.Length; oIndex > 0; --oIndex)
+            {
+                var nIndex = arr.Length - oIndex;
+
+                newArr[nIndex] = arr[oIndex - 1];
+            }
+
+            ShowOutPut(newArr);
+            return newArr;
+        }
+        
+    }
+}
--- ArraySept10/Program.cs
+++ ArraySept10/Program.cs
@@ -7,9 +7,28 @@ using System.Threading.Tasks;
 namespace ArraySept10
 {
     class Program
-    {
+    {   
         static void Main(string[] args)
         {
+            Console.WriteLine("Step 1: ");
+            ArrayPractice array = new ArrayPractice();
+            int[] firstArr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            array.Swap(firstArr, 0, 3);
+            array.ShowOutPut(firstArr);
+
+            Console.WriteLine("Step 2: ");
+            int[] secArr = new int[] { 1, 2, 3, 4, 5 };
+            array.ReverseInPlace(secArr);
+            array.ShowOutPut(secArr);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Step 3: ");
+            int[] thdArr = new int[] { 1, 2, 3, 4, 5, 6 };
+            array.ReverseNew(thdArr);
+
+            Console.WriteLine();
         }
     }
 }
